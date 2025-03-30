bc8c0decee3700f98cb5630d2aa3494e520abdc9
Elena Beschastnaya <beschastnaya99elena@gmail.com>
2025-03-30 23:11:10 +0300

    add new HM: homework7

---
+++ homework7/src/components/Board/GameBoard.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+
+import Cellss from './Cellss.vue';
+import { defineProps, defineEmits,computed } from 'vue';
+
+
+const emit = defineEmits<{
+    (e: 'clickMain', index: number): void;
+}>()
+
+
+
+const props = defineProps<{
+    cells: (string|null)[],
+    gameStatus: string
+}>()
+
+
+
+function clickUp(index:number) {
+    emit('clickMain', index)
+
+}
+
+const disabled = computed(() => {
+    console.log(disabled)
+    return props.gameStatus === 'игра идет' ;
+ 
+})
+
+</script>
+
+<template>
+    <div class="cells__cell">
+        <div class="cell">
+            <Cellss :disabled="!disabled" v-for="(cel, index) in cells" :cel="cel" :index="index" @clicks="clickUp" />
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+.cell {
+    display: flex;
+    flex-wrap: wrap;
+    width: 180px;
+    height: 180px;
+    gap: 3px;
+    justify-content: center;
+
+}
+</style>
